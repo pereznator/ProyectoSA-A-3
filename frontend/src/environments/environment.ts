693ba188023afde2fc0dafd4a502f57abcc0b114
jorge <jorgeperez@inlandlogistics.co>
2025-05-07 19:15:30 -0600

    environment updated

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: "true",
-  authServerUrl:"https://34.27.72.138",
-  productServerUrl:"https://34.27.72.138",
-  orderServiceUrl:"https://34.27.72.138",
-  chatServiceUrl: "https://34.27.72.138"
+  authServerUrl:"http://34.27.72.138",
+  productServerUrl:"http://34.27.72.138",
+  orderServiceUrl:"http://34.27.72.138",
+  chatServiceUrl: "http://34.27.72.138"
 };
